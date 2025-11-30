aceb015cb8392ed2973fd3f9876230db1c12e8b4
nhan.trinh <hongnhan50@gmail.com>
2025-11-30 21:12:50 +0700

    Restructure project. add supabase library

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from typing import Union
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+from loguru import logger
+from datetime import datetime
+import os
 
-from fastapi import FastAPI
+logger.add(f'/tmp/logs/{datetime.now().strftime("%Y-%m-%d")}.log',
+           backtrace=True,
+           diagnose=True,
+           colorize=True,
+           level='DEBUG',
+           rotation='00:00')
+
+load_dotenv('.env', verbose=True)
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+logger.debug(SUPABASE_URL)
+logger.debug(SUPABASE_ANON_KEY)
+
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="static/html")
 
+@app.get("/staffapp")
+async def staffapp(request: Request):
+    logger.info(f"Request: {request}")
+    return templates.TemplateResponse("staffapp.html",{"request":request})
+
+
+@app.get("/menu")
+async def menu(request: Request):
+    return templates.TemplateResponse("menu.html",{"request":request})
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+async def index(request: Request):
+    return templates.TemplateResponse("index.html",{"request":request})
